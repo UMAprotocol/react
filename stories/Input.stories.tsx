caef29811596b5f3fa06c7ae454d0a6df69472fb
Tulun <jaykiraly@gmail.com>
2021-12-22 15:44:41 -0800

    updated story.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -5,14 +5,14 @@ import { Input, InputProps } from "../src";
 const meta: Meta = {
   title: "Components/Input",
   component: Input,
-  // argTypes: {
-  //   variant: {
-  //     control: {
-  //       type: "radio",
-  //       options: ["primary", "outline"],
-  //     },
-  //   },
-  // },
+  argTypes: {
+    labelPlacement: {
+      control: {
+        type: "radio",
+        options: ["default", "overlap"],
+      },
+    },
+  },
 };
 
 export default meta;
@@ -20,20 +20,20 @@ export default meta;
 //👇 We create a “template” of how args map to rendering
 const Template: Story<InputProps> = args => {
   const [value, setValue] = useState("");
-  return (
-    <Input
-      width="400px"
-      placeholder="Type here..."
-      {...args}
-      value={value}
-      setValue={setValue}
-    />
-  );
+  return <Input {...args} value={value} setValue={setValue} />;
 };
 
 export const Basic = Template.bind({});
 
 Basic.args = {
-  // value: "primary",
-  // children: "Hello UMA",
+  width: "400px",
+  placeholder: "Type here...",
+};
+
+export const WithLabel = Template.bind({});
+
+WithLabel.args = {
+  width: "400px",
+  placeholder: "Type here...",
+  label: "Label for input",
 };
