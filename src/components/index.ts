237f1b20c088ff3744b0b199901a71c575d8c52b
Jason Kiraly <jaykiraly@gmail.com>
2021-12-24 09:04:31 -0800

    feature: Input in react lib (#3)
    * Input stub from umaverse.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * upgraded versions.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * Added basic storybook
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * bug in placeholder.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * updated story.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * Peer dependency for react and ts
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * update onChange function.
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>
    
    * refs for input
    
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Button, ButtonProps } from "./button";
-export { Select, SelectProps} from "./select";
+export { Select, SelectProps } from "./select";
+export { Input, InputProps } from "./input";
---
+++ stories/Input.stories.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Story, Meta } from "@storybook/react";
+import { Input, InputProps } from "../src";
+
+const meta: Meta = {
+  title: "Components/Input",
+  component: Input,
+  argTypes: {
+    labelPlacement: {
+      control: {
+        type: "radio",
+        options: ["default", "overlap"],
+      },
+    },
+  },
+};
+
+export default meta;
+
+//👇 We create a “template” of how args map to rendering
+const Template: Story<InputProps> = args => {
+  const [value, setValue] = useState("");
+  return (
+    <Input {...args} value={value} onChange={e => setValue(e.target.value)} />
+  );
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  width: "400px",
+  placeholder: "Type here...",
+};
+
+export const WithLabel = Template.bind({});
+
+WithLabel.args = {
+  width: "400px",
+  placeholder: "Type here...",
+  label: "Label for input",
+};
